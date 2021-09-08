8ca39bbb359b0a62289e008684482edc91c2996b
AzureAD\GabrielNegreanu <gabriel.negreanu@principal33.com>
2021-09-08 22:02:47 +0300

    Implemented add employee and delete employee

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 window.onload = () => {
-    document.getElementById("addEmployeeButton").addEventListener("click", AddEmployee, false);
+    document.getElementById("addEmployeeModal").addEventListener("click", AddEmployee, false);
     document.getElementById("openEmployeeModal").addEventListener("click", openModal, false);
+
+    document.querySelectorAll(".close-myModal").forEach(e => {
+        e.addEventListener("click", closeModal, false);
+    });
+
+    currentEmployees = JSON.parse(localStorage.getItem('employees'));
+
+    if (currentEmployees == undefined) {
+        localStorage.setItem('employees', JSON.stringify([]));
+        localStorage.setItem('employeeNextId', JSON.stringify(0));
+    } else {
+        currentEmployees.forEach(e => {
+            AppendTable(e);
+        });
+    }
+
+    setDelete();
 }
 
 //Open modal function
@@ -22,13 +39,61 @@ window.onclick = function(event) {
     }
 }
 
+function AppendTable(employee) {
+    tableContent = `<tr employeeId = ${employee.employeeId}>
+    <td>1</td>
+    <td><img src="${employee.picture}" class="picture"></td>
+    <td>${employee.Name}</td>
+    <td>${employee.email}</td>
+    <td>${employee.gender}</td>
+    <td>${employee.birthDate}</td>
+    <td><div class="delete-container"><a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a></div></td>
+    </tr>`
+    console.log(employee);
+    document.getElementById("tableEmployees").innerHTML += tableContent;
+}
 
-function AddEmployee() {​​​​​​​​
+function AddEmployee() {
     Name = document.getElementById("name-input").value;
     email = document.getElementById("email-input").value;
     gender = document.getElementById("gender-input").value;
     birthDate = document.getElementById("birthdate-input").value;
-    picture = document.getElementById("imgPreview").src;
+    // picture = document.getElementById("imgPreview").src;
+
+    employeeId = JSON.parse(localStorage.getItem('employeeNextId'));
+    allEmployees = JSON.parse(localStorage.getItem('employees'));
+
+    newEmployee = new Employee(employeeId++, Name, email, gender, birthDate, picture);
+    allEmployees.push(newEmployee);
+
+    localStorage.setItem('employeeNextId', JSON.stringify(employeeId));
+    localStorage.setItem('employees', JSON.stringify(allEmployees));
+
+    AppendTable(newEmployee);
+    setDelete();
+    closeModal();
+}
+
+//Delete event set on click
+function setDelete() {
+    document.querySelectorAll(".delete").forEach(e => {
+        e.addEventListener("click", deleteEmployeeRow, false);
+    });
+}
+
+//Delete employee function
+function deleteEmployeeRow(htmlDeleteElement) {
+    if (confirm('Are you sure to delete this employee?')) {
+        rowToBeDeleted = htmlDeleteElement.target.closest("tr");
+        employeeToDeleteId = rowToBeDeleted.getAttribute("employeeId");
+
+        rowToBeDeleted.remove();
+
+        allEmployees = JSON.parse(localStorage.getItem('employees'));
+        allEmployees = allEmployees.filter(e => e.employeeId != employeeToDeleteId);
+
+        localStorage.setItem('employees', JSON.stringify(allEmployees));
+    }
 }
 
 function Employee(employeeId, Name, email, gender, birthDate, picture) {
